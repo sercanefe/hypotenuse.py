287c67bbe7e87a06065b2fb610b275a528dadccb
Sercan Efe Karaman <70342545+sercanefe@users.noreply.github.com>
2021-10-01 00:09:40 +0300

    Add files via upload

---
+++ hipotenus.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 23:40:19 2021
+
+@author: sercan
+"""
+
+ilkKenar = int(input("ikinci kenarı yazınız: "))
+
+ikinciKenar = int(input("İkinci kenarı yazınız: "))
+
+hypotenuse = (ilkKenar**2 + ikinciKenar**2)**0.5
+
+print("Hipotenüs: ",hypotenuse)
